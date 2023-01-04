9cc0ab230c8ebe764e2290a6c1f75551d1cac7bd
nick lehman <nlehman@combitsystems.com>
2023-01-03 22:24:53 -0500

    Add save capability to localstorage
    - add mui icons dep
    - add Fab to force save
    - add SaveToLocalStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
@@ -8,14 +8,35 @@ import { useImmer } from "use-immer";
 import Box from "@mui/material/Box";
 import { ReactComponent as LeafIcon } from "./image2vector.svg";
 import SvgIcon from "@mui/material/SvgIcon";
+import Fab from "@mui/material/Fab";
+import SaveIcon from "@mui/icons-material/Save";
 
 function App() {
   const [state, setState] = useImmer({ sessionStart: new Date(), rows: [] });
   const turn = (e) => {
     setState((draft) => {
       draft.rows.push({ htm: new Date() });
+      saveToLocalStorage(draft);
     });
   };
+
+  const reset = () => {
+    setState((draft) => {
+      draft.sessionStart = new Date();
+      draft.rows = [];
+    });
+  };
+
+  const saveToLocalStorage = (newState) => {
+    window.localStorage.setItem(
+      `session-${newState.sessionStart.getTime()}`,
+      JSON.stringify(newState)
+    );
+  };
+
+  useEffect(() => {
+    console.log("updated");
+  });
 
   return (
     <Paper
@@ -24,6 +45,7 @@ function App() {
       sx={{
         p: 1,
         m: 0,
+        flexGrow: 1,
       }}
     >
       <Typeography variant="h3">Tractor</Typeography>
@@ -33,23 +55,68 @@ function App() {
       <Typeography variant="caption">
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={1}>
-          {state.rows.map((hit,i)=><Grid item key={i} xs={6}>{hit.htm.toISOString()}<SvgIcon sx={{color:"green"}}><LeafIcon/></SvgIcon></Grid>)}
+            {state.rows.map((hit, i) => (
+              <Grid item key={i} xs={6}>
+                {hit.htm.toISOString()}
+                <SvgIcon sx={{ color: "green" }}>
+                  <LeafIcon />
+                </SvgIcon>
+              </Grid>
+            ))}
           </Grid>
+          <Fab
+            variant="extended"
+            sx={{ position: "fixed", bottom: 64, mr: 1, right: 0 }}
+            onClick={saveToLocalStorage(state)}
+          >
+            <SaveIcon />
+            Save Session
+          </Fab>
         </Box>
       </Typeography>
-      <Button
-        variant="contained"
+      <Box
         sx={{
-          width: "100%",
-          minHeight: 200,
+          flexGrow: 1,
+          backgroundColor: "white",
           position: "fixed",
           bottom: 0,
-          left: 0, 
+          left: 0,
+          width: "100%",
+          borderTop: "1px solid grey",
+          padding: 0,
+          margin: 0,
         }}
-        onClick={turn}
       >
-        Hit
-      </Button>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+          sx={{ p: 1 }}
+        >
+          <Grid item xs={4}>
+            <Button
+              onClick={reset}
+              sx={{ width: "100%" }}
+              size="large"
+              variant="outlined"
+            >
+              Reset
+            </Button>
+          </Grid>
+          <Grid item xs={8}>
+            <Button
+              size="large"
+              sx={{ width: "100%" }}
+              variant="contained"
+              onClick={turn}
+            >
+              Hit
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </Paper>
   );
 }
